885227c326ef617e9a0de2c85eb2b68137b56a76
Geert-Johan Riemer <geertjohan@geertjohan.net>
2015-02-25 15:12:50 +0100

    Add comments and sleep 2 seconds.

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# echo commands
 set -x
 
+# kill any existing xpra-xeyes container
 docker stop -t 3 xpra-xeyes 2>&1 > /dev/null
 docker kill xpra-xeyes 2>&1 > /dev/null
 docker rm xpra-xeyes 2>&1 > /dev/null
 
+# build the container
 docker build -t xpra-xeyes .
 
+# start the container with .xpra socket folder bind-mounted.
+# capture the containerID, (which is the socket's hostname).
 CID=$(
 	docker run -d \
 		-v $HOME/.xpra:/home/user/.xpra \
@@ -15,8 +20,11 @@ CID=$(
 		xpra-xeyes
 )
 
-sleep 1
+# sleep is required because the container and xpra server might not be fully started otherwise
+sleep 2
 
+# attach to the xpra server
 XPRA_SOCKET_HOSTNAME="${CID:0:12}" xpra attach :100
 
+# stop the xpra server
 XPRA_SOCKET_HOSTNAME="${CID:0:12}" xpra stop :100
